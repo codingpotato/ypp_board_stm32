f353f0f7a6b871e966da3e27be574ed19d05b548
beanandbean <wangsw.a@gmail.com>
2018-09-04 18:53:38 +0800

    Use lto-enabled ar and ranlib

---
+++ nucleo_f767zi/system/config.h
@@ -0,0 +1,14 @@
+#ifndef YPP_BOARD_CONFIG_H
+#define YPP_BOARD_CONFIG_H
+
+#include <cstddef>
+
+namespace ypp {
+
+static constexpr std::size_t DEFAULT_STACK_SIZE = 1024;
+
+static constexpr std::size_t EXEC_CONTEXT_SIZE = 128; // arbitrary placeholder
+
+} // namespace ypp
+
+#endif /* YPP_BOARD_CONFIG_H */
